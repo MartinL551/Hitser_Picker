37cbbe2ef2e1237c6a9471e92a86330ceb6edc51
Martin Lever <levermartin555@gmail.com>
2025-12-09 14:25:44 +0000

    update: context working with first draft of hister data

--- src/screens/spinner/SpinnerScreen.tsx
+++ src/screens/spinner/SpinnerScreen.tsx
@@ -1,14 +1,40 @@
 import { hitserContext } from '@/store/HisterContext';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 export const SpinnerScreen = () => {
-  const { hitserValues } = React.useContext(hitserContext);
+  const { hitserValues, setHitserValues } = React.useContext(hitserContext);
 
   return (
     <View>
-      <Text>Context value: { hitserValues } </Text>
+      <Text>
+        Context value: 
+        { hitserValues.map((hitser, index) => <Button key={index} onPress={() => hideOnPress(index, hitserValues, setHitserValues)} title={hitser.name + hitser.show} />)}
+      </Text>
     </View>
   );
 };
+
+
+function hideOnPress(index, currentValues, setHitserValues) {
+    const initialHisterState = [
+      {
+          icon: 'rock',
+          name: 'Rock',
+          type: 'Rock',
+          show: true,
+      },
+      {
+          icon: 'pop',
+          name: 'Pop',
+          type: 'Pop',
+          show: false,
+      }
+  ]
+
+  console.log('updating state');
+
+  setHitserValues(initialHisterState)
+
+}
 
--- src/store/HisterContext.tsx
+++ src/store/HisterContext.tsx
@@ -1,6 +1,20 @@
 import * as React from "react";
 import { HitserItemsType } from "@/types/HitserItemsType";
 
+const initialHisterState = [
+    {
+        icon: 'rock',
+        name: 'Rock',
+        type: 'Rock',
+        show: true,
+    },
+    {
+        icon: 'pop',
+        name: 'Pop',
+        type: 'Pop',
+        show: true,
+    }
+]
 
 
 
@@ -8,7 +22,7 @@ export const hitserContext = React.createContext();
 
 export const HitserProvider = ({ children }) => {
 
-    const [hitserValues, setHitserValues] = React.useState('test');
+    const [hitserValues, setHitserValues] = React.useState(initialHisterState);
 
     return (
                 // this is the provider providing state
